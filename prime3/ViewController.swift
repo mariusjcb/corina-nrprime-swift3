0ff9d6f0e6d9e8714876c399e5aba4a375fc7cf3
Marius Ilie <marius_ilie20082001@yahoo.com>
2017-01-17 16:42:59 +0200

    Initial Commit

---
+++ prime3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  prime3
+//
+//  Created by Marius Ilie on 17/01/17.
+//  Copyright © 2017 University of Bucharest - Marius Ilie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
